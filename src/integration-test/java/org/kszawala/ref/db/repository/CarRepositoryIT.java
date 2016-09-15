facf687172750ad77b466877fa1ed382b5ec6fc1
kszawala <kszawala@poczta.fm>
2016-09-15 12:34:25 +0200

    CHG: renamed Integration Test class analogically to unit test class name.

--- src/integration-test/java/org/kszawala/ref/db/repository/CarRepositoryIT.java
+++ src/integration-test/java/org/kszawala/ref/db/repository/CarRepositoryIT.java
@@ -16,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-public class H2ReadApplicationIT extends AbstractTest {
+public class CarRepositoryIT extends AbstractTest {
 
 	@Autowired private CarRepository repository;
 
